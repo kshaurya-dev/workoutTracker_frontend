604e7540830dad57d0f31fff1b190c5993a6e808
unknown <kshaurya.dev@gmail.com>
2026-02-17 14:06:47 +0530

    env bugs solved

--- src/services/login.js
+++ src/services/login.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/login'
+const baseUrl = `${import.meta.env.VITE_API_URL}/api/login`
 const login = async (credentials) =>{
     const response  = await axios.post(baseUrl , credentials)
     return response.data
--- src/services/signup.js
+++ src/services/signup.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = 'http://localhost:3001/api/users'
+const baseUrl = `${import.meta.env.VITE_API_URL}/api/signup`
 
 const signup = async(body)=>{
     const response = await axios.post(baseUrl , body)
